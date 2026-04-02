3a8f847c634a02058f23b93a8ca971af4e15f1c2
Thiago Franco <Thiago@bigou.app>
2026-04-01 23:20:40 -0300

    refactor: remove hardcoded fallback Google Sheet IDs in sheets-proxy function

--- netlify/functions/sheets-proxy.ts
+++ netlify/functions/sheets-proxy.ts
@@ -22,13 +22,13 @@ const handler: Handler = async (event, _context) => {
     let sheetId = "";
     switch (type) {
         case "main":
-            sheetId = process.env.GOOGLE_SHEET_ID_MAIN || "1xmaLRjv7kbVjIdSlX0QiKzh2umRAGmLkia4LfYRT7aU";
+            sheetId = process.env.GOOGLE_SHEET_ID_MAIN || "";
             break;
         case "access":
-            sheetId = process.env.GOOGLE_SHEET_ID_ACCESS || "1fSmujBzlFtu4ZTuTl5v2nUcFwL3uol3QFqRrzEUULEA";
+            sheetId = process.env.GOOGLE_SHEET_ID_ACCESS || "";
             break;
         case "logo":
-            sheetId = process.env.GOOGLE_SHEET_ID_LOGO || "1Y5_TXSIi2RFyd_uUMXcWLQTQ52Oy8kCwYZrnlj6a5Xk";
+            sheetId = process.env.GOOGLE_SHEET_ID_LOGO || "";
             break;
         default:
             return {
